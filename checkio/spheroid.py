3d9917d858ecf5291a3d1d1e070821b6e0f7674c
Anton Melashchenko <scb-am@ukr.net>
2020-02-05 10:39:18 +0200

    add spheroid.py

---
+++ checkio/spheroid.py
@@ -0,0 +1,37 @@
+import math
+
+
+# def checkio(height, width):
+#     if width > height:
+#         V = 4 / 3 * math.pi * ((width/2)**2) * (height/2)
+#         e = math.sqrt(1 - height**2 / width**2)
+#         S = 0.5 * math.pi * (width**2) * (1 + (1-e**2)/e * math.atanh(e))
+#     elif width < height:
+#         V = 4 / 3 * math.pi * ((width / 2) ** 2) * (height / 2)
+#         e = math.sqrt(1 - width ** 2 / height ** 2)
+#         S = 0.5 * math.pi * (width**2) * (1 + (height / width / e * math.asin(e)))
+#     else:
+#         V = 4 / 3 * math.pi * ((width/2)**3)
+#         S = 4 * math.pi * ((width / 2) ** 2)
+#     print(f'V - {round(V, 2)}, S - {round(S, 2)}')
+
+
+checkio=lambda height,width:{
+    width>height:(lambda x,y:[
+        round(4/3*math.pi*((x/2)**2)*(y/2),2),
+        round((lambda x,e:0.5*math.pi*(x**2)*(1+(1-e**2)/e*math.atanh(e)))(x,math.sqrt(1-y**2/x**2)),2)
+    ] if x>y else False)(width,height),
+    width<height:(lambda x,y:[
+        round(4/3*math.pi*((x/2)**2)*(y/2),2),
+        round((lambda x, y, e:0.5*math.pi*(x**2)*(1+(y/x/e*math.asin(e))))(x,y,(math.sqrt(1-width**2/height**2))),2)
+    ] if x<y else False)(width,height),
+    width==height:(lambda x:[
+        round(4/3*math.pi*((x/2)**3),2),
+        round(4*math.pi*((x/2)**2),2)
+    ])(width)
+}[True]
+
+
+print(checkio(4, 2))
+print(checkio(2, 2))
+print(checkio(2, 4))
