50dedc94f80d5702c1e196815c7545ab11b067b1
Anton Melashchenko <brispol19@gmail.com>
2021-07-20 22:46:16 +0300

    add history to determine_order.py

--- checkio/determine_order.py
+++ checkio/determine_order.py
@@ -1,3 +1,17 @@
+"""
+The Robots have found an encrypted message. We cannot decrypt it at the moment,
+but we can take the first steps towards doing so. You have a set of "words",
+all in lower case, and each word contains symbols in "alphabetical order".
+(it's not your typical alphabetical order, but a new and different order.)
+We need to determine the order of the symbols from each "word" and create
+a single "word" with all of these symbols, placing them in the new alphabetical
+order. In some cases, if we cannot determine the order for several symbols,
+you should use the traditional latin alphabetical order . For example: Given
+words "acb", "bd", "zwa". As we can see "z" and "w" must be before "a" and "d"
+after "b". So the result is "zwacbd".
+"""
+
+
 def checkio(data):
     rules = []
     for word in data:
