9f71f67dd160c2b30f7518266186190257599717
Anton Melashchenko <41264608+scb-am@users.noreply.github.com>
2021-08-03 10:44:37 +0300

    Create palindromic_palindrome.py

---
+++ checkio/palindromic_palindrome.py
@@ -0,0 +1,54 @@
+"""
+Write a palindromic program with a checkio(s) function that checks whether s (a string) is a palindrome.
+
+For this task, using "#" is forbidden.
+
+You can use other methods for the function's definition (for example a lambda). The test will try to run the function "checkio" from your code.
+
+The example of the palindromic code:
+
+    checkio=lambda x: x#x :x adbmal=oikcehc
+1
+However, in your code, you can not use "#".
+
+Input: A text as a string.
+
+Output: Palindrome or not as a boolean.
+
+How it is used: This task is a challenge in creativity and is designed to show you the hidden depths of programming languages.
+
+Precondition: 1 < |text| ≤ 20
+The text contains only ASCII letters in lowercase.
+"""
+
+# Solution 1
+
+'''cba' ;]1-::[x == x :x adbmal = oikcehc
+'''
+checkio = lambda x: x == x[::-1]; 'abc'''
+
+
+# Solution 2
+
+"/m\" ;]1-::[x==x :x adbmal = oikcehc;";checkio = lambda x: x==x[::-1]; "\m/"
+
+
+# Solution 3
+
+''' '''\
+
+def checkio(s):return s == s[::-1]
+'''
+]1-::[s == s nruter:)s(oikcehc fed
+
+\''' '''
+
+
+# Solution 4
+
+"]1-::[s==s :s adbmal=oikcehcn\" cexe;""\n\"";exec("\ncheckio=lambda s: s==s[::-1]")
+
+
+# Solution 5
+
+" \"; ]1-::[s == s :s adbmal = oikcehc ;"; checkio = lambda s: s == s[::-1] ;"\ "
