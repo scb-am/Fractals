29a664e5ee5f2b515dc66fa0d564c6bdeae85de0
Anton Melashchenko <brispol19@gmail.com>
2022-03-30 14:47:20 +0300

    update text_editor.py

--- checkio/text_editor.py
+++ checkio/text_editor.py
@@ -31,6 +31,88 @@ class SavedText:
         self.versions.append(new_text)
 
 
+
+
+# class Text:
+#     text = font = ''
+#
+#     def write(self, text):
+#         self.text += text
+#
+#     def restore(self, old):
+#         self.text, self.font = old
+#
+#     def set_font(self, font):
+#         self.font = f'[{font}]'
+#
+#     def show(self):
+#         return f'{self.font}{self.text}{self.font}'
+#
+#
+# class SavedText(list):
+#     get_version = list.__getitem__
+#
+#     def save_text(self, text):
+#         self.append((text.text, text.font))
+
+
+
+
+# class Text:
+#     def __init__(self): self.restore(dict(contents='', font=None))
+#     def restore(self, version): vars(self).update(version)
+#     def write(self, text): self.contents += text
+#     def set_font(self, font): self.font = font
+#     def show(self):
+#         fontrep = '' if self.font is None else self.font.join('[]')
+#         return fontrep + self.contents + fontrep
+#
+# class SavedText(list):
+#     def save_text(self, text): self.append(vars(text).copy())
+#     get_version = list.__getitem__
+
+
+
+
+# from copy import copy
+#
+#
+# class Text:
+#
+#     def __init__(self):
+#         self.font = None
+#         self.text = []
+#
+#     def write(self, text):
+#         self.text.append(text)
+#
+#     def set_font(self, font):
+#         self.font = font
+#
+#     def show(self):
+#         text = ''.join(self.text)
+#         if self.font is not None:
+#             return '[{0}]{1}[{0}]'.format(self.font, text)
+#         return text
+#
+#     def restore(self, version):
+#         self.text = version[0]
+#         self.font = version[1]
+#
+#
+# class SavedText:
+#
+#     def __init__(self):
+#         self.versions = []
+#
+#     def save_text(self, text):
+#         version = (copy(text.text), text.font)
+#         self.versions.append(version)
+#
+#     def get_version(self, number):
+#         return self.versions[number]
+
+
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
 
