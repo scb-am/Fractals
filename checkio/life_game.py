15b312a8e15b61ee453faa1e5f8196d43a1c0b88
Anton Melashchenko <brispol19@gmail.com>
2022-04-03 15:20:57 +0300

    update life_game.py

--- checkio/life_game.py
+++ checkio/life_game.py
@@ -155,6 +155,74 @@ def life_counter(map: tuple, tick_n: int):
     return life_game.get_alive_cells_count()
 
 
+
+
+# from collections import Counter
+# from itertools import chain, product
+#
+# def life_counter(state, ticks):
+#     cells = product(range(len(state)), range(len(state[0])))
+#     lives = {(y, x) for y, x in cells if state[y][x] == 1}
+#
+#     neighbors = lambda c: set(product(*[(z - 1, z, z + 1) for z in c])) - {c}
+#     for t in range(ticks):
+#         freqs = Counter(chain.from_iterable(map(neighbors, lives)))
+#         lives = {c for c in freqs if freqs[c] in (3 - (c in lives), 3)}
+#
+#     return len(lives)
+
+
+
+
+# from collections import Counter
+# from functools import lru_cache
+# from itertools import chain, product
+#
+# capacities = {True: (2, 3), False: (3,)}
+#
+# @lru_cache(maxsize=256)
+# def neighbors(c):
+#     return set(product(*[(z - 1, z, z + 1) for z in c])) - {c}
+#
+# def life_counter(state, ticks):
+#     cells = product(range(len(state)), range(len(state[0])))
+#     lives = {(y, x) for y, x in cells if state[y][x]}
+#
+#     for t in range(ticks):
+#         freqs = Counter(chain.from_iterable(map(neighbors, lives)))
+#         lives = {c for c, f in freqs.items() if f in capacities[c in lives]}
+#
+#     return len(lives)
+
+
+
+
+# def life_counter(s,t,E=enumerate):
+#  L={(i,j)for i,r in E(s)for j,c in E(r)if c}
+#  for _ in[0]*t:L={z for z,n in __import__("collections").Counter((a+c,b+d)for
+# a,b in L for c in{-1,0,1}for d in{-1,0,1}).items()if(z in L)+4>n>2}
+#  return len(L)
+
+
+
+
+# from collections import Counter
+# from itertools import chain
+#
+#
+# def life_counter(state, tick_n):
+#     l = {(y, x) for y in range(len(state))
+#          for x in range(len(state[0])) if state[y][x]}
+#     for _ in range(tick_n):
+#         l = {c for c, f in Counter(chain.from_iterable(
+#             ((y - 1, x - 1), (y - 1, x), (y - 1, x + 1), (y, x - 1),
+#              (y, x + 1), (y + 1, x - 1), (y + 1, x), (y + 1, x + 1))
+#             for y, x in l)).items() if f in ((3,), (2, 3))[c in l]}
+#     return len(l)
+
+
+
+
 # from collections import Counter
 #
 # def life_counter(state, tick_n):
@@ -170,6 +238,59 @@ def life_counter(map: tuple, tick_n: int):
 #         live_cells = {cell for cell, count in neighbours.items()
 #                       if count == 3 or count == 4 and cell in live_cells} #live +1 for itself
 #     return len(live_cells)
+
+
+
+
+# def life_counter(state, tick_n):
+#     live = {(x, y) for y in range(len(state)) for x in range(len(state[0])) if state[y][x]}
+#
+#     for _ in range(tick_n):
+#
+#         next_live = set()
+#         born_candidate = set()
+#
+#         # Any live cell with two or three live neighbours lives on to the next generation.
+#         for x, y in live:
+#             neighbor = {(x + i, y + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not i == j == 0}
+#             if len(neighbor & live) in (2, 3): next_live |= {(x, y)}
+#             born_candidate |= neighbor - live
+#
+#         # Any dead cell with exactly three live neighbours becomes a live cell.
+#         for x, y in born_candidate:
+#             neighbor = {(x + i, y + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not i == j == 0}
+#             if len(neighbor & live) == 3: next_live |= {(x, y)}
+#
+#         live = next_live
+#
+#     return len(live)
+
+
+
+
+# def life_counter(state, tick_n):
+#     # Convert the initial state to a set of tuples of coordinates of alive cells.
+#     alive = set((r, c) for r, row in enumerate(state)
+#                 for c, cell in enumerate(row) if cell)
+#     # Convenience varaible: all possible offsets to nearby cells, including itself.
+#     square = [(r, c) for r in [-1, 0, 1] for c in [-1, 0, 1]]
+#
+#     # Returns a set of all the neighbours of currently alive cells.
+#     def extension(alive):
+#         return set((row + r, col + c) for row, col in alive for r, c in square)
+#
+#     def is_alive(row, col, alive):
+#         # Note that for code simplicity we count a cell as its own neigbour here and
+#         # adjust the condition for an alive cell to remain alive correspondingly.
+#         # when checking the number of alive neighbours of an alive cells.
+#         n_neighbours = sum((row + r, col + c) in alive for r, c in square)
+#         return n_neighbours in [3, 4] if (row, col) in alive else n_neighbours == 3
+#
+#     for tick in range(tick_n):
+#         alive = set((r, c) for r, c in extension(alive) if is_alive(r, c, alive))
+#
+#     return len(alive)
+
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
