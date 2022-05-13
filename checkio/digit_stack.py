e871def5dd5674ef2f4e67d0f512e2236c042a02
Anton Melashchenko <brispol19@gmail.com>
2022-05-13 15:47:29 +0300

    update digit_stack.py

--- checkio/digit_stack.py
+++ checkio/digit_stack.py
@@ -15,6 +15,35 @@ def digit_stack(commands):
     return sum(summ)
 
 
+# def digit_stack(commands):
+#     stack, digitsum = [], 0
+#     for command in commands:
+#         if command.startswith("PUSH"):
+#             stack.append(int(command[-1]))
+#         elif stack:
+#             digitsum += stack[-1]
+#             if command == "POP":
+#                 stack.pop()
+#     return digitsum
+
+
+# def digit_stack(cs, s=__import__('re').sub):
+#     fix = lambda f,c,n: c if n == c else fix(f, f(c), c)
+#     return eval('0' + s(r'(\d)', r'+\1', s(r'(POP|PEEK|PUSH\d)', r'',
+#         fix (lambda x: s(r'PUSH(\d)(\d*)PEEK', r'PUSH\1\2\1',
+#         s(r'PUSH(\d)(\d*)POP', r'\1\2', x)), s(' ', '', ''.join(cs)), ''))))
+
+
+# digit_stack=d=lambda c,a='':len(c)and(d(c[1:],c[0][5]+a)if' 'in c[0]else(len(a)and int(a[0]))+d(c[1:],a['O'in c[0]:]))
+
+
+# def digit_stack(cmds, s=[]):
+#     e, s = {"PU": lambda x: s.append(x.split()[1]) == 1,
+#             "PO": lambda x: s.pop() if s else 0,
+#             "PE": lambda x: s[len(s)-1] if s else 0}, []
+#     return sum([int(e[x[:2]](x)) for x in cmds])
+
+
 if __name__ == '__main__':
     print("Example:")
     print(digit_stack(["PUSH 3", "POP", "POP", "PUSH 4", "PEEK",
